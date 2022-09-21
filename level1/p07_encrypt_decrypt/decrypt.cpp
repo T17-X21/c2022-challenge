754534c5b4f0897dece0b4f9af47999dbf285dde
T17-X21 <2194671691@qq.com>
2022-09-21 12:09:56 +0800

    完成了 encrypt

---
+++ level1/p07_encrypt_decrypt/decrypt.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+	char str[1001];
+	int key;
+	gets_s(str);
+	scanf_s("%d", &key);
+	for (int i=0; i < strlen(str); i++) {
+		if (str[i] >= 'A' && str[i] <= 'Z') {
+			str[i] = (str[i] - 'A' - key+26) % 26 + 'A';
+		}
+		else
+			if (str[i] >= 'a' && str[i] <= 'z') {
+				str[i] = (str[i] - 'a' - key+26) % 26 + 'a';
+			}
+			else
+				if (str[i] >= '0' && str[i] <= '9') {
+					str[i] = (str[i] - '0' - key+10) % 10 + '0';
+				}
+	}
+	puts(str);
+	return 0;
+}
