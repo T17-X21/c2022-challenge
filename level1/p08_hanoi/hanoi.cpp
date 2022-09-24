b4b93b56b1aaaa5bc5c402420b65af7bc08852e8
T17-X21 <2194671691@qq.com>
2022-09-25 00:12:05 +0800

    完成了 hanoi

---
+++ level1/p08_hanoi/hanoi.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void hanoi(int n, char a, char b, char c)
+{
+	if (n == 0) {
+		return;
+	}
+	hanoi(n - 1, a, c, b);
+	printf("%c -> %c\n", a, c);
+	hanoi(n - 1, b, a, c);
+}
+int main()
+{
+	int n;
+	scanf_s("%d", &n);
+	hanoi(n, 'A', 'B', 'C');
+	return 0;
+}
